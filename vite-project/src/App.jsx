cdafa547420e7e0767bb6bb063b574cf19d1d1a1
Mokhtar Ouidah <mokhtarouidah@Mokhtars-iMac.local>
2023-06-05 14:36:23 -0400

    u

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,23 @@
+import React, { useState } from 'react'
 import React from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
